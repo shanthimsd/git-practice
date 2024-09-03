ffe5a3cd991d9ea0d31193e9ac34a18f2e4a0b89
shanthimsd <madasushanthi7@gmail.com>
2024-09-03 21:55:10 +0800

    Datatypes

---
+++ 04-datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+STUDENTS=("ntr" "ram" "gopal" "varma" "charan")
+echo "First studet is : ${STUDENTS[0]}"
+echo "Second studet is : ${STUDENTS[1]}"
+echo "Third studet is : ${STUDENTS[2]}"
+echo "Fourth studet is : ${STUDENTS[3]}"
+echo "Fifth studet is : ${STUDENTS[4]}"
+echo "All studets in the class : ${STUDENTS[@]}"
+
