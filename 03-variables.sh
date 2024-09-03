12e1fba23139ebbe25778f732339a9e67ee5aac3
shanthimsd <madasushanthi7@gmail.com>
2024-09-03 21:44:31 +0800

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hii $person2 , How are you"
+echo "$person2:: Yeah $person1 ,im good, how you doing?"
+echo "$person1:: everyting going well $person2, nothing special is happening."
+echo "$person2:: for me same."
