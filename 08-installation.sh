249ac5d80c556f1296e0055f9b5f86b3b727e53f
shanthimsd <madasushanthi7@gmail.com>
2024-09-05 15:32:36 +0800

    shell

--- 08-installation.sh
+++ 08-installation.sh
@@ -27,3 +27,23 @@ then
     else 
         echo "Git is already installed, nothing to do"
     fi
+
+dnf installed mysql
+
+if [ $? -ne 0 ]
+then 
+ echo "mysql is not installed, going to install it.."
+
+ dnf install mysql -y
+
+ if [ $? -ne 0 ]
+ then 
+  echo "mysql intallation is not sucess..check it."
+  exit 1
+  
+  else
+   echo "mysql installation is sucesses"
+  fi
+  else
+   echo "mysql is already installed nothing to do."
+   fi
