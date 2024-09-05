4df830465dea28ee0d9c9e5d5c84df2950032d49
shanthimsd <madasushanthi7@gmail.com>
2024-09-05 14:10:56 +0800

    shell

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-STUDENTS=("ntr" "ram" "gopal" "varma" "charan")
+STUDENTS=("ntr"
+   "ram" 
+   "gopal"
+    "varma"
+     "charan")
 echo "First studet is : ${STUDENTS[0]}"
 echo "Second studet is : ${STUDENTS[1]}"
 echo "Third studet is : ${STUDENTS[2]}"
@@ -8,3 +12,8 @@ echo "Fourth studet is : ${STUDENTS[3]}"
 echo "Fifth studet is : ${STUDENTS[4]}"
 echo "All studets in the class : ${STUDENTS[@]}"
 
+Number1=$1
+Number2=$2
+SUM=$(($Number1+$Number2))
+
+echo "Total of Number1 and Number2 is: $SUM"
