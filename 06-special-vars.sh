4992849fdf253862568794a7217d3ffe3ef5fe6b
shanthimsd <madasushanthi7@gmail.com>
2024-09-05 14:36:15 +0800

    shell

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -3,7 +3,7 @@
 echo "All variables passed to the script: $@ "
 echo "Number of the variables passed: $# "
 echo "Script name: $0 "
-echo "Current working directory: $pwd "
+echo "Current working directory: $PWD "
 echo "Home directery of the user: $HOME "
 echo "PID of the script executing now: $$ "
 sleep 100 &
