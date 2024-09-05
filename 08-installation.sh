e120832239e176424077b2555dbc2f6b7428c4fe
shanthimsd <madasushanthi7@gmail.com>
2024-09-05 15:27:21 +0800

    shell

--- 08-installation.sh
+++ 08-installation.sh
@@ -11,10 +11,12 @@ fi
 
 dnf list installed git #just checking installed or not
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 then 
     echo "Git is not installed, going to intall it.."
+    
     dnf install git -y
+
     if [ $? -ne 0 ]
     then 
         echo "Git installation is not sucess..check it"
