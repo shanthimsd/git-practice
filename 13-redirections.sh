a2247ad1f1194481bf3280053bb44d7d8bf367b9
shanthimsd <madasushanthi7@gmail.com>
2024-09-06 12:09:19 +0800

    shell

---
+++ 13-redirections.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME+$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y=%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+UserId=$(id -u)
+R="\e=[31m"
+G="\e=[32m"
+N="\e=[0m"
+
+Check_Root(){
+    if [ $id -ne 0 ]
+    then 
+        echo -e "$R Please run the script with root priveliges $N"
+        exit 1
+    fi
+}
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.. $R Failed $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is.. $G Sucesses $N" &>>$LOG_FILE
+    fi
+}
+Check_Root
